e4555a5f84bbd2d38c7604625ea0cc53955646b9
Greg Lindberg <greglindberg@gmail.com>
2023-11-01 12:48:33 -0700

    updated README, documented pdf_amazon_extract.py

--- README.md
+++ README.md
@@ -24,23 +24,24 @@ pip install -r requirements.txt
 
 
 ## Usage
-To run the project, put your bank statements in the corresponding folder, navigate to the project directory and run the following command:
+To run the project, put your VISA card and/or Amazon Invoice PDF statements in the corresponding folders, navigate to the project directory and run the following command(s):
 
 python pdf_bofa_extract.py
 python pdf_chase_extract.py
+python pdf_amazon_extract.py
 
 
 ## Modules Used
 - `os` for operating system dependent functionalities.
 - `re` for regular expressions.
 - `pandas` for data manipulation and analysis.
-- `PyPDF2` for PDF file reading.
+- `PyPDF2`,`PDFPlumber` for PDF file reading.
 - `datetime` for manipulating dates and times.
 
 ## To-Do
 - [ ] Add support for Wells Fargo statements.
-- [ ] Add support for Amazon purchase records.
-- [ ] Implement AI analysis for categorizing all transactions for tax purposes.
+- [X] Add support for Amazon purchase records.
+- [ ] Implement AI analysis for categorizing all transactions based on descriptions.
 
 ## Contributing
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
--- pdf_amazon_extract.py
+++ pdf_amazon_extract.py
@@ -1,3 +1,21 @@
+"""
+This module is designed to extract specific data points from Amazon invoices in PDF format.
+It saves the extracted data into CSV and XLSX files.
+
+Functions:
+    - clean_currency_string: Cleans and converts a currency string to a float.
+    - isolate_and_structure_item_descriptions: Extracts and structures item descriptions.
+    - extract_amazon_fields: Extracts basic order fields like date, number, and total.
+    - extract_all_under_items_ordered: Captures all text under 'Items Ordered'.
+    - extract_gift_card_amount: Extracts gift card amount if applicable.
+    - extract_amazon_invoice_data: Main function to compile all data.
+    - save_to_files: Saves extracted data to CSV and XLSX files.
+    - clean_keys: Cleans dictionary keys.
+
+Usage:
+    >>> python pdf_amazon_extract.py
+"""
+
 import re
 import os
 import pdfplumber
@@ -5,17 +23,35 @@ import pprint
 import pandas as pd
 import csv
 
-SOURCE_DIR = "SourceStatements/AmazonTests"
+SOURCE_DIR = "SourceStatements/Amazon"
 OUTPUT_PATH_CSV = "ConsolidatedReports/Amazon_all.csv"
 OUTPUT_PATH_XLSX = "ConsolidatedReports/Amazon_all.xlsx"
 
 
 def clean_currency_string(currency_str):
+    """
+    Cleans and converts a currency string to a float.
+
+    Parameters:
+        currency_str (str): The currency string to clean. E.g., "$1,000.50"
+
+    Returns:
+        float: The cleaned currency as a float. E.g., 1000.50
+    """
     return float(currency_str.replace("$", "").replace(",", ""))
 
 
 # Modifying the function to remove residual text 'Other Condition: New' from the 'Description'
 def isolate_and_structure_item_descriptions(captured_string):
+    """
+    Extracts and structures item descriptions from the text captured under "Items Ordered".
+
+    Parameters:
+        captured_string (str): The string containing item descriptions.
+
+    Returns:
+        list: A list of dictionaries containing structured item descriptions.
+    """
     item_list = []
     item_data = {}
     # Split the captured string by item quantity to separate individual items
@@ -92,11 +128,20 @@ def isolate_and_structure_item_descriptions(captured_string):
 
 
 def extract_amazon_fields(pdf_text):
+    """
+    Extracts basic order information like order placed date, order number, and order total.
+
+    Parameters:
+        pdf_text (str): Text extracted from the Amazon invoice PDF.
+
+    Returns:
+        dict: Dictionary containing basic order information.
+    """
     extracted_data = {}
     fields_to_extract = ["Order Placed:", "order number:", "Order Total:"]
 
     for field in fields_to_extract:
-        match = re.search(f"{field} (.*?)\n", pdf_text)
+        match = re.search(f"(?i){field} (.*?)\n", pdf_text)
         if match:
             # Remove commas if the field is 'Order Total:'
             if field == "Order Total:":
@@ -110,6 +155,15 @@ def extract_amazon_fields(pdf_text):
 
 # Function to extract all text under 'Items Ordered' into a single string for post-processing
 def extract_all_under_items_ordered(pdf_text):
+    """
+    Captures all the text under the "Items Ordered" section in the Amazon invoice PDF.
+
+    Parameters:
+        pdf_text (str): Text extracted from the Amazon invoice PDF.
+
+    Returns:
+        str: Text captured under "Items Ordered".
+    """
     lines = pdf_text.split("\n")
     all_text = []
     capture_mode = False
@@ -127,6 +181,15 @@ def extract_all_under_items_ordered(pdf_text):
 
 
 def extract_gift_card_amount(last_page_text):
+    """
+    Extracts the gift card amount used in the transaction, if applicable.
+
+    Parameters:
+        pdf_text (str): Text extracted from the Amazon invoice PDF.
+
+    Returns:
+        float: Gift card amount used in the transaction, or None if not applicable.
+    """
     gift_card_amount = "0"  # Initialize with a default value of 0
     gift_card_match = re.search(r"Gift Card Amount:-\$(\d+\.\d+)", last_page_text)
     if gift_card_match:
@@ -136,6 +199,15 @@ def extract_gift_card_amount(last_page_text):
 
 # Main function to extract all desired data from an Amazon invoice PDF
 def extract_amazon_invoice_data(pdf_path):
+    """
+    Main function to invoke other functions and compile all extracted data.
+
+    Parameters:
+        pdf_path (str): File path of the Amazon invoice PDF.
+
+    Returns:
+        dict: Dictionary containing all extracted data points.
+    """
     # Initialize an empty list to store the text from each page
     amazon_invoice_text_pdfplumber = []
     extracted_data = {}
@@ -172,6 +244,17 @@ def extract_amazon_invoice_data(pdf_path):
 
 # Function to save the master list to CSV and XLSX files
 def save_to_files(master_list):
+    """
+    Saves the extracted data to CSV and XLSX files.
+
+    Parameters:
+        extracted_data (dict): Dictionary containing all extracted data points.
+        csv_path (str): File path for saving the CSV file.
+        xlsx_path (str): File path for saving the XLSX file.
+
+    Returns:
+        None
+    """
     # Determine all possible keys from all dictionaries
     all_keys = set()
     for item in master_list:
@@ -191,6 +274,20 @@ def save_to_files(master_list):
     df.to_excel(OUTPUT_PATH_XLSX, index=False)
 
 
+def clean_keys(input_dict):
+    """
+    Cleans the keys in the dictionaries by removing colons and capitalizing.
+
+    Parameters:
+        data_dict (dict): Dictionary with keys to be cleaned.
+
+    Returns:
+        dict: Dictionary with cleaned keys.
+    """
+    return {key.replace(":", "").title(): value for key, value in input_dict.items()}
+
+
+# Main Processor Loop
 # Initialize a master list to store all item details from all PDFs
 master_list = []
 
@@ -208,15 +305,9 @@ for file_name in os.listdir(SOURCE_DIR):
         master_list.append(item_details)
 
 
-pprint.pprint(master_list)
-print(f"MasterList Quantity: {len(master_list)}")
-# Save the master list to CSV and XLSX files
-save_to_files(master_list)
+cleaned_list = [clean_keys(item) for item in master_list]
+pprint.pprint(cleaned_list)
+print(f"MasterList Quantity: {len(cleaned_list)}")
 
-
-# Example usage
-# pdf_path = "SourceStatements/Amazon/Amazon.com - Order 112-1503364-8255416.pdf"
-# extracted_data = extract_amazon_invoice_data(pdf_path)
-
-# pprint.pprint(extracted_data, indent=4)
-# print(extracted_data)
+# Save the cleaned master list to CSV and XLSX files
+save_to_files(cleaned_list)
